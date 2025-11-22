003944feea110246a4dfba810eb61b975babacf1
jmauriciouni <jamauriciosalazar7@gmail.com>
2025-11-22 13:51:44 -0500

    redirect to api docs page

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
-# api/main.py
+# en api/main.py
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 from .config import API_TITLE, API_VERSION
 from .routes import api_router
@@ -10,6 +11,11 @@ def create_app() -> FastAPI:
         title=API_TITLE,
         version=API_VERSION,
     )
+
+    @app.get("/", include_in_schema=False)
+    def root():
+        return RedirectResponse(url="/docs")
+
     app.include_router(api_router)
     return app
 
